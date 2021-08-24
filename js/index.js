eb09cea9edca4f100f3b51ea33f2d7ca98775757
DESKTOP\kosolapov <aleksejkosolapov999@gmail.com>
2021-08-24 20:43:39 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+﻿const gameSize = 4;
+
+const listImages = [
+    "images/ball.png",
+    "images/bike.png",
+    "images/car.png",
+    "images/crab.png",
+    "images/dog.png",
+    "images/dolphin.png",
+    "images/plane.png",
+    "images/sun.png"
+];
+
+function initGame() {
+    // .. код
+}
+
+initGame();
