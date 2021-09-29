03225a373e7cbdda6cd7350a84817d60a579d934
DESKTOP\kosolapov <aleksejkosolapov999@gmail.com>
2021-09-29 20:25:34 +0300

    added flip cards

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 ﻿const gameSize = 4;
-
+//Массив картинок
 const listImages = [
     "images/ball.png",
     "images/bike.png",
@@ -18,18 +18,67 @@ const listImages = [
     "images/plane.png",
     "images/sun.png"
 ];
+//Перемешаный объект картинок
+const listImagesShuffle = {
+    cell0:"",
+    cell1:"",
+    cell2:"",
+    cell3:"",
+    cell4:"",
+    cell5:"",
+    cell6:"",
+    cell7:"",
+    cell8:"",
+    cell9:"",
+    cell10:"",
+    cell11:"",
+    cell12:"",
+    cell13:"",
+    cell14:"",
+    cell15:"",
+};
 
-function initGame() {
+//Объект с рубашками
+const arrayShirt = {
+    cell0:"images/shirt.png",
+    cell1:"images/shirt.png",
+    cell2:"images/shirt.png",
+    cell3:"images/shirt.png",
+    cell4:"images/shirt.png",
+    cell5:"images/shirt.png",
+    cell6:"images/shirt.png",
+    cell7:"images/shirt.png",
+    cell8:"images/shirt.png",
+    cell9:"images/shirt.png",
+    cell10:"images/shirt.png",
+    cell11:"images/shirt.png",
+    cell12:"images/shirt.png",
+    cell13:"images/shirt.png",
+    cell14:"images/shirt.png",
+    cell15:"images/shirt.png",
+};
+
+function shufImg() {
     shuffle(listImages);
     for(let i = 0; i < 16; i++){
-        let cellId = "cell" + (i+1) ;
+        let a = "cell"+i;
+       listImagesShuffle [a] = listImages [i];
+    }
+    return listImagesShuffle;
+}
+//заполняет таблицу картинками или рубашками
+function initGame() {
+    //shuffle(listImages);
+    for(let i = 0; i < 16; i++){
+        let cellId = "cell" + i;
         let img = document.createElement('img');
-        img.src = listImages[i];
+        //img.src = listImages[i];
+        img.src = arrayShirt[cellId];
         let parentElement = document.getElementById(cellId);
         parentElement.appendChild(img);
     }
 }
-
+//Перемешивает массив
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
     
@@ -45,5 +94,28 @@ function shuffle(array) {
     return array;
 }
 
+function onklickFunction(clickId) {
+    for(let i = 0; i < 16; i++){
+        let cellId = "cell" + i;
+        for(let j = 0; j < 16; j++){
+            let deleteElement = document.getElementById(cellId);
+            deleteElement.innerHTML ="";
+        }
+        if(clickId == cellId){
+            let img = document.createElement('img');
+            //img.src = listImages[i];
+            img.src = listImagesShuffle[cellId];
+            let parentElement = document.getElementById(cellId);
+            parentElement.appendChild(img);
+        }else{
+            let img = document.createElement('img');
+            //img.src = listImages[i];
+            img.src = arrayShirt[cellId];
+            let parentElement = document.getElementById(cellId);
+            parentElement.appendChild(img);
+        }
+    }
+}
 initGame();
+console.log(shufImg());
 
